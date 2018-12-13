92af43e0666d52763bd336c81241388f40483a85
=DamjanRadev <radevdamjan@gmail.com>
2018-12-13 19:43:13 +0100

    Completed proposals form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -7,17 +9,20 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     DocumentsComponent,
-    ProposalListComponent
+    ProposalListComponent,
+    ProposalNewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Document } from './document'
 @Component({
-  
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
