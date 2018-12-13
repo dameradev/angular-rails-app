180fadc7af384330d997352d38c77ab9be4d746b
=DamjanRadev <radevdamjan@gmail.com>
2018-12-13 23:14:23 +0100

    Added card styles for displyaing documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -12,21 +12,21 @@ export class DocumentsComponent {
       description: "desc for document",
       file_url: "https://google.com",
       updated_at: "13/12/18",
-      image_url: "https://google.com",
+      image_url: "https://artisantalent.com/wp-content/uploads/2012/04/mistakes-to-avoid-when-hiring-freelancers.jpg",
     },
     {
       title: "My Second doc",
       description: "desc for document",
       file_url: "https://google.com",
       updated_at: "13/12/18",
-      image_url: "https://google.com",
+      image_url: "https://artisantalent.com/wp-content/uploads/2012/04/mistakes-to-avoid-when-hiring-freelancers.jpg",
     },
     {
       title: "My Third doc",
       description: "desc for document",
       file_url: "https://google.com",
       updated_at: "13/12/18",
-      image_url: "https://google.com",
+      image_url: "https://artisantalent.com/wp-content/uploads/2012/04/mistakes-to-avoid-when-hiring-freelancers.jpg",
     }
   ]
 }
